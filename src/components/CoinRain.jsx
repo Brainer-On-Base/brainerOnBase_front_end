8b1dca0f6c5ed3829925b732a656289e90cde501
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-05-02 21:09:41 +0200

    edd effect

--- src/components/CoinRain.jsx
+++ src/components/CoinRain.jsx
@@ -3,31 +3,51 @@ import Coin from "./Coin";
 
 const CoinRain = () => {
     const [coins, setCoins] = useState([]);
+
     const calcRandom = () => (Math.random(0.5) - 0.5) * 12
 
     useEffect(() => {
-        if (coins.length === 0) {
-            const newCoins = [...Array(200)].map((_, index) => ({
-                key: index,
-                position: [
-                    calcRandom(),
-                    calcRandom(),
-                    -1
-                ],
-                scale: [0.5, 0.2, 0.5],
-                rotation: [
-                    Math.random() * Math.PI,
-                    Math.random() * Math.PI,
-                    0
-                ],
-            }));
-            setCoins(newCoins);
-        }
-    }, [coins.length]);
+        const newCoins = [...Array(200)].map((_, index) => ({
+            key: index,
+            position: [
+                calcRandom(),
+                calcRandom(),
+                -1
+            ],
+            scale: [0.5, 0.2, 0.5],
+            rotation: [
+                Math.random() * Math.PI,
+                Math.random() * Math.PI,
+                0
+            ],
+            velocity: [
+                0, // No hay velocidad lateral
+                0, // No hay velocidad lateral
+                -0.02 * Math.random() - 0.01 // Velocidad vertical
+            ]
+        }));
+        setCoins(newCoins);
+    }, []);
 
+    useEffect(() => {
+        const animate = () => {
+            setCoins(prevCoins => prevCoins.map(coin => ({
+                ...coin,
+                position: coin.position.map((pos, index) => {
+                    let newPos = pos + coin.velocity[index];
+                    if (index === 2 && newPos < -1) {
+                        newPos = 10;
+                    }
+                    return newPos;
+                })
+            })));
+            requestAnimationFrame(animate);
+        };
+        animate();
+    }, []);
 
     return (
-        <>
+        <group>
             {coins.map((coin) => (
                 <Coin
                     key={coin.key}
@@ -36,7 +56,7 @@ const CoinRain = () => {
                     rotation={coin.rotation}
                 />
             ))}
-        </>
+        </group>
     );
 };
 
