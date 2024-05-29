220639c22a086a796a9868bf4aff1ba682d3ca37
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-05-29 22:48:37 +0200

    routes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,20 @@ import WelcomeSection from "../components/home/WelcomeSection";
 import Footer from "../components/Footer";
 import TokenomicsSection from "../components/home/TokenomicsSection";
 import HistorySection from "../components/home/HistorySection";
+import { useLocation } from "react-router-dom";
 
 export default function Home() {
 
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.getElementById(location.hash.substring(1));
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [location]);
 
   return <StyledAppContainer>
       <Navbar/>
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,22 @@
 import { Routes as ReactRoutes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import App from '../pages/App';
+import BrainerOnBaseGame from '../components/game/BrainerOnBaseGame';
+import BrainerOnBaseShop from '../components/shop/BrainerOnBaseShop';
 
 
 const Routes = () => {
     return (
         <ReactRoutes >
             <Route path="/home" element={<Home />} />
+            <Route
+                exact path={'/game'}
+                element={<BrainerOnBaseGame/>}
+            />
+            <Route
+                exact path={'/shop'}
+                element={<BrainerOnBaseShop/>}
+            />
             <Route
                 exact path={'/'}
                 element={<App/>}
