7030136d74f23e24face6ad59f7a82428714a215
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-05-29 22:48:26 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { StyledFlexCenterContainer, StyledNavbarContainer } from "./styled-components/container";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTelegram, faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Navbar() {
     const navigate = useNavigate()
@@ -13,9 +13,14 @@ export default function Navbar() {
                 <img src="./brainerCoin.png" onClick={() => navigate('/home')} style={{cursor:'pointer'}}/>
                 <a href="/">BRAINER</a>
             </StyledFlexCenterContainer>
-            <StyledFlexCenterContainer style={{width: 'auto', margin: '2em'}}>
-                <FontAwesomeIcon icon={faTelegram} size="1x" title="Join our Telegram"/>
-                <FontAwesomeIcon onClick={() => window.open('https://twitter.com/BrainerOnBase')} icon={faTwitter} size="1x" style={{background: 'transparent'}} title="Follow on twitter"/>
+            <StyledFlexCenterContainer style={{width: 'auto'}}>
+                <a onClick={()=> navigate('#history')}>About</a>
+                <a onClick={() => navigate('#bubbletokenomics')}>Tokenomics</a>
+                <a onClick={() => navigate('#swap')}>Swap</a>
+                <a onClick={() => navigate('/shop')}>Merch</a>
+                <a onClick={() => navigate('/game')}>Game</a>
+                {/* <FontAwesomeIcon icon={faTelegram} size="1x" title="Join our Telegram"/>
+                <FontAwesomeIcon onClick={() => window.open('https://twitter.com/BrainerOnBase')} icon={faTwitter} size="1x" style={{background: 'transparent'}} title="Follow on twitter"/> */}
             </StyledFlexCenterContainer>
         </StyledNavbarContainer>)
 }
