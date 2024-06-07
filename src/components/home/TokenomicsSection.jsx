8bce078601ac7fdb99c717ea219f39417983e820
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-06-07 18:52:23 +0200

    Update TokenomicsSection.jsx

--- src/components/home/TokenomicsSection.jsx
+++ src/components/home/TokenomicsSection.jsx
@@ -11,7 +11,7 @@ const { isVisible } = handleAnimationScroll('.target')
     <StyledCard className={`animate__animated ${isVisible[0] ? 'animate__backInLeft' : ''} target`} style={{visibility: isVisible[0] ? 'visible' : 'hidden'}}>
       <div>
         <h2>LIQUIDITY & MARKETS</h2>
-        <p style={{marginTop: '-40px', marginBottom: '2em'}}>60% - 6.000.000.000</p>
+        <p style={{marginTop: '-40px', marginBottom: '2em'}}>65% - 6.500.000.000</p>
         <p>Liquidity and accessibility! We allocate a significant portion of our token supply to ensure ample liquidity on exchanges. 
         This facilitates <span>SMOOTH TRADING EXPERIENCES</span> for our users and supports the growth of our token in various markets.</p>
       </div>
@@ -21,18 +21,17 @@ const { isVisible } = handleAnimationScroll('.target')
     <StyledCard className={`animate__animated ${isVisible[1] ? 'animate__backInRight' : ''} target`} style={{visibility: isVisible[1] ? 'visible' : 'hidden'}}>
       <img src="./rewards2.png"/>
       <div>
-        <h2>COMMUNITY & REWARDS</h2>
-        <p style={{marginTop: '-40px', marginBottom: '2em'}}>15% - 1.500.000.000</p>
-        <p>Join our <span>COMMUNITY</span> and participate in <span>AIRDROPS</span>! Explore various ways to <span>EARN TOKENS</span> creating engaging content, participating in contests, or spreading the word about our project. Be part of our <span>growing ecosystem</span>.</p>
+        <h2>PLAY & EARN</h2>
+        <p style={{marginTop: '-40px', marginBottom: '2em'}}>15% - 1,500,000,000</p>
+        <p>Join our <span>SOCIETY</span> and dive into our <span>GAME</span>! Discover a exciting way to <span>EARN TOKENS</span> by playing in our society. <span>INVITE</span> friends and explore our <span>WORLD</span>. </p>
       </div>
-
     </StyledCard>
     <StyledCard className={`animate__animated ${isVisible[2] ? 'animate__backInLeft' : ''} target`} style={{visibility: isVisible[2] ? 'visible' : 'hidden'}}>
       <div>
         <h2>DEVELOPMENT TEAM</h2>
         <p style={{marginTop: '-40px', marginBottom: '2em'}}>10% - 1.000.000.000</p>
-        <p> Meet our dedicated development team! Our talented developers are committed to <span>BUILDING</span> and <span>IMPROVING</span> our project. 
-            With years of <span>EXPERIENCE</span> in the industry, they work tirelessly to deliver innovative solutions and drive the success of our platform.</p>
+        <p> Our talented developers are committed to <span>BUILDING</span> and <span>IMPROVING</span> our <span>SOCIETY</span>. 
+            With years of <span>EXPERIENCE</span> in the industry, they work tirelessly to deliver innovative solutions and drive the success of our <span>WORLD</span>.</p>
       </div>
       <img src="./development.png"/>
     </StyledCard>
@@ -40,7 +39,7 @@ const { isVisible } = handleAnimationScroll('.target')
     <img src="./marketingBrain.png"/>
       <div>
         <h2>MARKETING & PROMOTION</h2>
-        <p style={{marginTop: '-40px', marginBottom: '2em'}}>10% - 1.000.000.000</p>
+        <p style={{marginTop: '-40px', marginBottom: '2em'}}>5% - 500.000.000</p>
         <p>Spread the word and be part of our journey! <span>Marketing</span> plays a crucial role in increasing awareness and adoption of our project. 
           From social media campaigns to <span>STRATEGIC PARTNERSHIPS</span>, we invest in various marketing initiatives to reach new audiences and promote <span>OUR VISION</span>.</p>
       </div>
