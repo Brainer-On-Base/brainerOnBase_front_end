9198d571d7abb3a8a9cef014cdf63b95ae244d48
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-09-04 13:11:24 +0200

    Update HistorySection.jsx

--- src/components/home/HistorySection.jsx
+++ src/components/home/HistorySection.jsx
@@ -1,44 +1,38 @@
-import React, { useEffect, useState } from "react";
-import { StyledAstronautCard, StyledAstronautCard1, StyledAstronautCard2, StyledAstronautCard3, StyledFlexFullCenterContainer, StyledHistorySection } from "../styled-components/container";
-import BubbleDialog from "../BubbleDialog";
-import { Canvas } from "@react-three/fiber";
+import React from "react";
+import { StyledAstronautCard1, StyledAstronautCard2, StyledAstronautCard3, StyledFlexFullCenterContainer, StyledHistorySection } from "../styled-components/container";
 import NightBackground from "../NIghtBackground";
 import { View } from "@react-three/drei";
-import handleAnimationScroll from "../../hooks/handleAnimationScroll";
 
 
 export default function HistorySection() {
-  const {isVisible} = handleAnimationScroll('.animations')
 
-  
-  return <StyledHistorySection id="history">
-          <View
-        style={{ width: '100%', height: '100%', position: 'absolute', zIndex: 1, top: 0, bottom: 0 }}
-        >
-
-        <NightBackground/>
+return <StyledHistorySection id="history">
+    <View
+      style={{ width: '100%', height: '100%', position: 'absolute', zIndex: 1, top: 0, bottom: 0 }}
+    >
+      <NightBackground/>
     </View>
     <StyledFlexFullCenterContainer style={{height: 'auto', flexDirection:'column'}}>
-        <h1  className={`animate__animated ${isVisible[0] ? 'animate__fadeInDown' : ''} animations`} style={{visibility: isVisible[0] ? 'visible' : 'hidden'}}>ABOUT</h1>
-        <img src="./history.png" className={`animate__animated ${isVisible[1] ? 'astronaut' : ''} animations`} style={{visibility: isVisible[1] ? 'visible' : 'hidden'}} />
+        <h1  className={`animate__animated animate__fadeInDown animations`} >ABOUT</h1>
+        <img src="./history.png" className={`animate__animated astronaut animations`} />
     </StyledFlexFullCenterContainer>
 
-    <StyledAstronautCard1 className={`animate__animated ${isVisible[2] ? 'animate__backInLeft' : ''} animations`} style={{visibility: isVisible[2] ? 'visible' : 'hidden'}}>
+    <StyledAstronautCard1 className={`animate__animated animate__backInLeft animations`} >
       <h2>4% TAX</h2>
       <p> Funds collected are directed towards <span>BUYBACKS</span>, ensuring liquidity and facilitating <span>FUTURE LISTINGS</span> to enhance its global accessibility and utility. </p>
     </StyledAstronautCard1>
-    <StyledAstronautCard2 className={`animate__animated ${isVisible[3] ? 'animate__backInRight' : ''} animations`} style={{visibility: isVisible[3] ? 'visible' : 'hidden'}}>
+    <StyledAstronautCard2 className={`animate__animated animate__backInRight animations`} >
       <h2>WE ARE A FAMILY</h2>
       <p>We prioritize your <span>VOICE</span> and <span>IDEAS</span> above all. Join us in building a vibrant community where every member plays a vital role in our journey to the <span>MOON</span>.</p>
     </StyledAstronautCard2>
-    <StyledAstronautCard3 className={`animate__animated ${isVisible[3] ? 'animate__backInUp' : ''} animations`} style={{visibility: isVisible[3] ? 'visible' : 'hidden'}}>
+    <StyledAstronautCard3 className={`animate__animated animate__backInUp animations`} >
       <h2>LOCKED LP</h2>
       <p> Our locked liquidity pool feature <span>SAFEGUARDS</span> investor assets by securely locking liquidity, promoting <span>CONFIDENCE</span> and <span>STABILITY</span> in the market.</p>
     </StyledAstronautCard3>
 
 
 
-    <img src="./spaceship.png"  className={"img1"} />
+    {/* <img src="./spaceship.png"  className={"img1"} />
       <BubbleDialog
         id="bubbletokenomics"
         active={true}
@@ -53,6 +47,6 @@ export default function HistorySection() {
       >
           <p>Take a look to our <span>TOKENOMICS</span> to clarify your doubts!</p>
           <p> We are a <span>REAL</span> community  token!</p>
-      </BubbleDialog>
+      </BubbleDialog> */}
     </StyledHistorySection>;
 }
