7fccdd53f15e1f7ad2302635c63e28c10e88c2e6
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-12-21 14:20:23 +0100

    Update Routes.jsx

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,33 +1,20 @@
-import { Routes as ReactRoutes, Route } from 'react-router-dom';
-import Home from '../pages/Home';
-import App from '../pages/App';
-import BrainerOnBaseShop from '../components/shop/BrainerOnBaseShop';
-import BrainerOnBaseMain from '../components/game/BrainerOnBaseMain';
-import NftDetails from '../pages/NftDetails';
-
+import { Routes as ReactRoutes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import App from "../pages/App";
+import BrainerOnBaseShop from "../components/shop/BrainerOnBaseShop";
+import BrainerOnBaseMain from "../components/game/BrainerOnBaseMain";
+import NftDetails from "../pages/NftDetails";
 
 const Routes = () => {
-    return (
-        <ReactRoutes >
-            <Route path="/home" element={<Home />} />
-            <Route
-                exact path={'/game'}
-                element={<BrainerOnBaseMain/>}
-            />
-            <Route
-                exact path={'/shop'}
-                element={<BrainerOnBaseShop/>}
-            />
-            <Route
-                exact path={'/nft-details/:id'}
-                element={<NftDetails/>}
-            />
-            <Route
-                exact path={'/'}
-                element={<App/>}
-            />
-        </ReactRoutes>
-    )
+  return (
+    <ReactRoutes>
+      <Route path="/home" element={<Home />} />
+      <Route path={"/game"} element={<BrainerOnBaseMain />} />
+      <Route path={"/shop"} element={<BrainerOnBaseShop />} />
+      <Route path={"/nft-details/:id"} element={<NftDetails />} />
+      <Route path={"/"} element={<App />} />
+    </ReactRoutes>
+  );
 };
 
 export default Routes;
