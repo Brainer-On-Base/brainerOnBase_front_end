d4c833735a4d3bdd6b81c38efaa844a7eba55b67
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2025-02-15 13:30:26 +0100

    Update WelcomeSection.jsx

--- src/components/home/WelcomeSection.jsx
+++ src/components/home/WelcomeSection.jsx
@@ -1,61 +1,77 @@
 import React, { useEffect, useState } from "react";
-import { StyledAddressContainer, StyledFlexCenterContainer, StyledWelcomeSection2 } from "../styled-components/container";
+import {
+  StyledAddressContainer,
+  StyledFlexCenterContainer,
+  StyledWelcomeSection2,
+} from "../styled-components/container";
 import { StyledButton } from "../styled-components/buttons";
 import useModals from "../../hooks/useSweetAlert";
 import { APP_TEXTS } from "../../APP_TEXTS";
-import { ethers } from 'ethers';
+import { ethers } from "ethers";
 import UseContract from "../../hooks/useContract";
+import { motion } from "framer-motion";
 
 export default function WelcomeSection() {
-  const {showPopUp, useTextModal} = useModals()
-  const { mint_BPC1_NFT, getMintedCount, web3provider } = UseContract()
-  const [mintedCount, setMintedCount] = useState(null)
+  const { showPopUp, useTextModal } = useModals();
+  const { mint_BPC1_NFT, getMintedCount, web3provider } = UseContract();
+  const [mintedCount, setMintedCount] = useState(null);
 
   useEffect(() => {
     const fetchMintedCount = async () => {
-        const count = await getMintedCount();
-        setMintedCount(count);
+      const count = await getMintedCount();
+      setMintedCount(count);
     };
     fetchMintedCount();
-}, [getMintedCount]);
+  }, [getMintedCount]);
 
+  return (
+    <StyledWelcomeSection2 style={{ alignItems: "flex-start" }} id="home">
+      {/* <img src="rocket.png" className="baloon"/> */}
 
-  return <StyledWelcomeSection2 style={{alignItems:'flex-start'}} id="home">
+      <div className="home-text-container">
+        <div>
+          <h1 className={"animate__animated animate__backInDown"}>BRAINER</h1>
+          <h1
+            className={
+              "animate__animated animate__fadeInDown animate__delay-1s"
+            }
+          >
+            N BASE
+          </h1>
+          <h1
+            className={"animate__animated animate__fadeIn animate__delay-1s "}
+          >
+            ARE YOU A NO <span>BRAINER</span>?
+          </h1>
+          <StyledFlexCenterContainer className={"welcome-actions"}>
+            <StyledButton
+              size={"2em"}
+              className={"animate__animated animate__fadeIn animate__delay-2s"}
+              onClick={() =>
+                useTextModal({
+                  textButton: APP_TEXTS.HOME_MODAL_TEXT_BUTTON,
+                  title: APP_TEXTS.HOME_MODAL_TITLE,
+                  text: APP_TEXTS.HOME_MODAL_DESCRIPTION,
+                  textColor: "white",
+                  onConfirmFunction: async () => await mint_BPC1_NFT(),
+                })
+              }
+            >
+              {APP_TEXTS.HOME_BUTTON_1}
+            </StyledButton>
+            {web3provider && (
+              <p className="animate__animated animate__fadeIn animate__delay-2s minted-quantity">{`${mintedCount}/8000 minted`}</p>
+            )}
+          </StyledFlexCenterContainer>
 
-    {/* <img src="rocket.png" className="baloon"/> */}
-
-    <div className="home-text-container">
-      <div>
-        <h1 className={'animate__animated animate__backInDown'}>BRAINER</h1>
-        <h1 className={'animate__animated animate__fadeInDown animate__delay-1s'}>N BASE</h1>
-        <h1 className={'animate__animated animate__fadeIn animate__delay-1s '}>ARE YOU A NO <span>BRAINER</span>?</h1>
-        <StyledFlexCenterContainer className={'welcome-actions'}>
-
-        <StyledButton 
-        size={'2em'} 
-        className={'animate__animated animate__fadeIn animate__delay-2s'}
-        onClick={() => useTextModal({
-          textButton: APP_TEXTS.HOME_MODAL_TEXT_BUTTON,
-          title:APP_TEXTS.HOME_MODAL_TITLE,
-          text: APP_TEXTS.HOME_MODAL_DESCRIPTION, 
-          textColor: 'white',
-          onConfirmFunction:  async () =>  await mint_BPC1_NFT()
-        })} 
-        
-        >
-          {APP_TEXTS.HOME_BUTTON_1}
-        </StyledButton>
-        {web3provider && <p className="animate__animated animate__fadeIn animate__delay-2s minted-quantity">{`${mintedCount}/8000 minted`}</p>}
-        </StyledFlexCenterContainer>
-
-        {/* <StyledButton 
+          {/* <StyledButton 
         onClick={()=>  showPopUp({text:'Liquidity pool is not available yet', icon:'warning'})}
         size={'2em'} className={'animate__animated animate__fadeIn animate__delay-2s'}>
           CHARTS
         </StyledButton> */}
+        </div>
+        <img src="moon.png" className="moon" />
       </div>
-      <img src="moon.png" className="moon"/>
-
-    </div>
-  </StyledWelcomeSection2>;
+    </StyledWelcomeSection2>
+  );
 }
