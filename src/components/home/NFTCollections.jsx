84fdc72a8ef5195cbb168a1101894152c10e2344
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2025-02-26 10:02:29 +0100

    Update NFTCollections.jsx

--- src/components/home/NFTCollections.jsx
+++ src/components/home/NFTCollections.jsx
@@ -140,7 +140,7 @@ const NFTCollections = () => {
                 icon: "./commonBrainer.png",
               },
               {
-                text: "BE PART OF THE HISTORY: Owning a Pixel Brainer is a symbol of your support for the rise of Brainers everywhere and you will receive exclusive benefits in the future.",
+                text: "BE PART OF THE HISTORY: Owning a Pixel Brainer is a symbol of your support for the journey of Brainers to the moon and you will receive exclusive benefits in the future.",
                 icon: "./commonBrainer.png",
               },
             ].map((item, index) => (
