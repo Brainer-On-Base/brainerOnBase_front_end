54a5ed14cdee7b13efa2435b09d9ea2e4de45487
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2025-03-04 08:24:15 +0100

    Update BubbleDialog.jsx

--- src/components/BubbleDialog.jsx
+++ src/components/BubbleDialog.jsx
@@ -2,19 +2,22 @@ import React from "react";
 import { StyledBubbleDialog } from "./styled-components/container";
 
 export default function BubbleDialog({
-    active,
-    children,
-    positionStyle,
-    positionDialogSpik,
-    width= '200px',
-    ...props
+  active,
+  children,
+  positionStyle,
+  positionDialogSpik,
+  width = "200px",
+  ...props
 }) {
-
-
   return (
-  <StyledBubbleDialog position={positionStyle} width={width} positionspik={positionDialogSpik} {...props}
-  style={{display: active ? 'block' : 'none'}}
-  >
-        {children}
-  </StyledBubbleDialog>);
+    <StyledBubbleDialog
+      position={positionStyle}
+      width={width}
+      positionspik={positionDialogSpik}
+      {...props}
+      style={{ display: active ? "block" : "none", overflow: "hidden" }}
+    >
+      {children}
+    </StyledBubbleDialog>
+  );
 }
