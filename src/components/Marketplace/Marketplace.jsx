c5690610bed2661adc1051dfd92478aeec01ab50
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2025-03-06 08:20:45 +0100

    Create Marketplace.jsx

---
+++ src/components/Marketplace/Marketplace.jsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from "react";
+import { HBox } from "../../HocComponents";
+import MarketplaceNavbar from "./MarketplaceNavbar";
+import MarketplaceFiltersBar from "./MarketplaceFiltersBar";
+
+const items = [
+  {
+    id: 1,
+    name: "Golden Throne",
+    category: "Furniture & Decoration",
+    price: 500,
+    image: "placeholder.jpg",
+  },
+  {
+    id: 2,
+    name: "Mystic Cape",
+    category: "Clothing & Accessories",
+    price: 300,
+    image: "placeholder.jpg",
+  },
+  {
+    id: 3,
+    name: "Cyber Dog",
+    category: "Pets & Accessories",
+    price: 800,
+    image: "placeholder.jpg",
+  },
+  {
+    id: 4,
+    name: "Energy Drink",
+    category: "Food & Drinks",
+    price: 50,
+    image: "placeholder.jpg",
+  },
+  {
+    id: 5,
+    name: "Teleport Crystal",
+    category: "Special & Functional Items",
+    price: 1000,
+    image: "placeholder.jpg",
+  },
+  {
+    id: 6,
+    name: "Rare Collectible Coin",
+    category: "Collectibles & Exclusives",
+    price: 2000,
+    image: "placeholder.jpg",
+  },
+  {
+    id: 7,
+    name: "Speed Boost",
+    category: "Boosts & Power-ups",
+    price: 150,
+    image: "placeholder.jpg",
+  },
+];
+
+const Marketplace = () => {
+  const [categorySelected, setCategorySelected] = useState("All");
+  const [subCategorySelected, setSubCategorySelected] = useState("All");
+
+  useEffect(() => {
+    if (categorySelected === "All") {
+      setSubCategorySelected("All");
+    }
+  }, [categorySelected]);
+
+  return (
+    <HBox width="100%" align="flex-start">
+      <MarketplaceNavbar
+        setCategorySelected={setCategorySelected}
+        categorySelected={categorySelected}
+        setSubCategorySelected={setSubCategorySelected}
+      />
+      <HBox
+        width="75%"
+        background={"shadePurpleDark"}
+        padding="20px"
+        borderRadius="10px"
+        height="100%"
+        direction="column"
+      >
+        <MarketplaceFiltersBar categorySelected={categorySelected} />
+        {items.map((item) => (
+          <HBox key={item.id} direction="column" align="center">
+            <img src={item.image} alt={item.name} />
+            <p>{item.name}</p>
+            <p>{item.category}</p>
+            <p>{item.price}</p>
+          </HBox>
+        ))}
+      </HBox>
+    </HBox>
+  );
+};
+
+export default Marketplace;
