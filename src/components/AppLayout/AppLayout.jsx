8cf7dda67492828256e8c1ba6eb5253a28ed6242
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2025-03-09 17:17:36 +0100

    Update AppLayout.jsx

--- src/components/AppLayout/AppLayout.jsx
+++ src/components/AppLayout/AppLayout.jsx
@@ -3,11 +3,20 @@ import { StyledAppContainer } from "../styled-components/container";
 import Navbar from "../Navbar/Navbar";
 import { View } from "@react-three/drei";
 import NightBackground from "../NIghtBackground";
-import { HBox } from "../../HocComponents";
+import { HBox, HButton } from "../../HocComponents";
 import GenericTitle from "../GenericTitle/GenericTitle";
 import Footer from "../Footer";
+import UseContract from "../../hooks/useContract";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const AppLayout = ({ title, children }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const isFromSocietyLogic =
+    location.pathname === "/marketplace" ||
+    location.pathname === "/profile" ||
+    location.pathname === "/inventory";
+  const { isConnected } = UseContract();
   return (
     <StyledAppContainer>
       <Navbar />
@@ -31,7 +40,60 @@ const AppLayout = ({ title, children }) => {
             // subtitleFontSize={"40px"}
           />
         )}
-
+        {isConnected && isFromSocietyLogic && (
+          <HBox
+            width="100%"
+            align="center"
+            justify="center"
+            padding="5px"
+            margin="-30px 0 0 0"
+            overflowX="hidden"
+            direction="row"
+          >
+            <HButton
+              fontSize={"30px"}
+              padding={"10px 20px"}
+              style={{
+                zIndex: 99999,
+                border:
+                  location.pathname === "/profile"
+                    ? "1px solid #ff437d"
+                    : "transparent",
+              }}
+              onClick={() => navigate("/profile")}
+            >
+              PROFILE
+            </HButton>
+            <HButton
+              fontSize={"30px"}
+              padding={"10px 20px"}
+              style={{
+                zIndex: 99999,
+                border:
+                  location.pathname === "/inventory"
+                    ? "1px solid #ff437d"
+                    : "transparent",
+              }}
+              onClick={() => navigate("/inventory")}
+            >
+              INVENTORY
+            </HButton>
+            <HButton
+              fontSize={"30px"}
+              padding={"10px 20px"}
+              style={{
+                zIndex: 99999,
+                border:
+                  location.pathname === "/marketplace"
+                    ? "1px solid #ff437d"
+                    : "transparent",
+              }}
+              onClick={() => navigate("/marketplace")}
+            >
+              MARKETPLACE
+            </HButton>
+          </HBox>
+        )}
         {children}
       </HBox>
       <Footer />
