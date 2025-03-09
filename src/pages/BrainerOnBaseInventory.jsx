37a4af86e6acb06d59976c03ccb5ff27e56139df
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2025-03-09 17:17:55 +0100

    add layouts

--- src/pages/BrainerOnBaseInventory.jsx
+++ src/pages/BrainerOnBaseInventory.jsx
@@ -1,14 +1,7 @@
 import React, { useEffect, useContext } from "react";
-import { StyledAppContainer } from "../components/styled-components/container";
-import { View } from "@react-three/drei";
-import NightBackground from "../components/NIghtBackground";
-import Navbar from "../components/Navbar/Navbar";
 import { useNavigate } from "react-router-dom";
 import AccountContext from "../provider/AccountProvider/AccountContext";
-import { HBox } from "../HocComponents";
-import GenericTitle from "../components/GenericTitle/GenericTitle";
-import Footer from "../components/Footer";
-import Inventory from "../components/Inventory/Inventory";
+import Inventory from "../components/society/inventory/Inventory";
 import AppLayout from "../components/AppLayout/AppLayout";
 
 const BrainerOnBaseInventory = () => {
--- src/pages/BrainerOnBaseSociety.jsx
+++ src/pages/BrainerOnBaseSociety.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import AppLayout from "../components/AppLayout/AppLayout";
 
 const BrainerOnBaseSociety = () => {
-  return <div></div>;
+  return <AppLayout title="BRAINER" subtitle="SOCIETY"></AppLayout>;
 };
 
 export default BrainerOnBaseSociety;
