3c681d47e3c3b4974d76524db34215559f7ce0f9
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2025-04-16 12:49:10 +0200

    Create BrainerOnBaseSociety.jsx

---
+++ src/pages/BrainerOnBaseSociety.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import { HBox } from "../HocComponents";
+import AppLayout from "../components/AppLayout/AppLayout";
+import BrainerSociety from "../components/society/society/BrainerSociety";
+
+const BrainerOnBaseSociety = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    const element = document.getElementById("society");
+    element.scrollIntoView({ behavior: "smooth" });
+  }, []);
+  return (
+    <AppLayout title="SOCIETY" id={"society"}>
+      <BrainerSociety />
+    </AppLayout>
+  );
+};
+
+export default BrainerOnBaseSociety;
