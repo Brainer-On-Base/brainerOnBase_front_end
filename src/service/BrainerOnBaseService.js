ed40b1dea1dd79ca2f5bca1791ce95f100ab3591
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2025-05-08 10:49:34 +0200

    Update BrainerOnBaseService.js

--- src/service/BrainerOnBaseService.js
+++ src/service/BrainerOnBaseService.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 import { pixelBrainerCollectionEndpoints } from "./PixelBrainerCollection";
 
-export const API_BASE_URL = "http://localhost:5000/api"; // Cambia esto si tu backend tiene otra URL base
+export const API_BASE_URL =
+  import.meta.env.VITE_ENVIRONMENT === "production"
+    ? import.meta.env.API_BASE_URL
+    : "http://localhost:5000/api"; // Cambia esto si tu backend tiene otra URL base
 
 const BrainerOnBaseService = {
   ...pixelBrainerCollectionEndpoints,
